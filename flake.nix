1040a81b632dc80e2a26d71144ccf10054ec4859
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-03-23 16:44:25 -0600

    roughly added docker, need to nixify it

--- flake.nix
+++ flake.nix
@@ -6,18 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        web-ii-project = pkgs.stdenv.mkDerivation {
+          name = "web-ii-project";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.nodejs_23 ];
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r ./ $out/
+            cd $out
+            npm install --omit=dev
+          '';
+        };
       in {
         devShell = pkgs.callPackage ./nix/devShell.nix {};
         formatter = pkgs.alejandra;
+
+        packages = {
+          web-ii-project = web-ii-project;
+          default = web-ii-project;
+        };
+
+        defaultPackage = web-ii-project;
       }
     );
 }

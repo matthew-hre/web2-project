cd5990cfa223986370beda03c6575e00e5c6ff6c
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-03-23 17:14:40 -0600

    opted into not nixifying

--- flake.nix
+++ flake.nix
@@ -6,33 +6,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        web-ii-project = pkgs.stdenv.mkDerivation {
-          name = "web-ii-project";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.nodejs_23 ];
-
-          installPhase = ''
-            mkdir -p $out
-            cp -r ./ $out/
-            cd $out
-            npm install --omit=dev
-          '';
-        };
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
       in {
         devShell = pkgs.callPackage ./nix/devShell.nix {};
         formatter = pkgs.alejandra;
-
-        packages = {
-          web-ii-project = web-ii-project;
-          default = web-ii-project;
-        };
-
-        defaultPackage = web-ii-project;
       }
     );
 }
